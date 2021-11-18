4a969d5378c7314b02aa5347ffdc4fcc6a07075b
Ramso <Ramso@DESKTOP-F436ANM>
2021-11-18 17:34:24 +0100

    Proyecto Cuenta creado con atributos y metodos definidos

---
+++ src/Cuenta.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Ramso
+ */
+public class Cuenta {
+    private double saldo;
+    private String numCuenta;
+    private String dniTitular;
+    
+    Cuenta(double saldo, String numCuenta, String dniTitular){
+    
+    }
+    
+    public void retirar(double cantidad){
+    
+    }
+    public void ingresar(double cantidad){
+    
+    }
+    public double getSaldo(){
+        return 0.0;
+    }
+    public String getNumCuenta(){
+        return "";
+    }
+    
+}
